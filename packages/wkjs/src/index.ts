7c29de6af9d5084c9971660b4d2ac82568ba555d
Ross Hendry <rhendry@googlemail.com>
2019-08-12 18:33:02 +0100

    Pass stages through as parameter

--- packages/wkjs/src/index.ts
+++ packages/wkjs/src/index.ts
@@ -2,7 +2,7 @@ import { assignments, reviewStatistics, subject } from './api'
 import { WanikaniAPI } from './types/external'
 
 const wkjs = (apiKey: string): WanikaniAPI => ({
-  assignments: () => assignments(apiKey),
+  assignments: (stages: number[]) => assignments(apiKey, stages),
   reviewStatistics: () => reviewStatistics(apiKey),
   subject: (id: number) => subject(apiKey, id),
 })
